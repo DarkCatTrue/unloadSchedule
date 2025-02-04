69719f4d783adc1e128ff381aee0d112e3a9238c
DarkCat <Nallblack06264@gmail.com>
2025-02-04 17:28:15 +0900

    Добавил кнопку для отображения пароля в настройках, сделал сохранение конфигурации в json

---
+++ JsonConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace unloadSchedule
+{
+    public class JsonConfiguration
+    {
+        public string schedulePath { get; set; }
+        public string Ip { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public JsonConfiguration(string path, string ip, string login, string password)
+        {
+            schedulePath = path;
+            Ip = ip;
+            Login = login;
+            Password = password;
+        }
+    }
+
+}
--- Pages/settingsPage.xaml.cs
+++ Pages/settingsPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace unloadSchedule
 {
@@ -7,6 +10,8 @@ namespace unloadSchedule
     /// </summary>
     public partial class settingsPage : Page
     {
+        ImageSourceConverter imgs = new ImageSourceConverter();
+
         public settingsPage()
         {
             InitializeComponent();
@@ -16,6 +21,39 @@ namespace unloadSchedule
         {
             Page main = new Main();
             MainWindow.pageManager.ChangePage(main);
+        }
+
+        private void PassIcon_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (textBox.Visibility == System.Windows.Visibility.Visible)
+            {
+                textBox.Visibility = System.Windows.Visibility.Collapsed;
+                passwordBox.Visibility = System.Windows.Visibility.Visible;
+                PassIcon.SetValue(Image.SourceProperty, imgs.ConvertFromString("pack://application:,,,/Icons/HiddenPassBtn.png"));
+            }
+            else
+            {
+                passwordBox.Visibility = System.Windows.Visibility.Collapsed;
+                textBox.Visibility = System.Windows.Visibility.Visible;
+                PassIcon.SetValue(Image.SourceProperty, imgs.ConvertFromString("pack://application:,,,/Icons/showPassBtn.png"));
+            }
+        }
+
+        private void passwordBox_PasswordChanged(object sender, System.Windows.RoutedEventArgs e)
+        {
+            textBox.Text = passwordBox.Password;
+        }
+
+        private void saveBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            string filepath = @"\configuration.json";
+            string path = pathBox.Text;
+            string ip = ipBox.Text;
+            string login = loginBox.Text;
+            string password = passwordBox.Password;
+            JsonConfiguration configuration = new JsonConfiguration(path, ip, login, password);
+            string json = JsonConvert.SerializeObject(configuration);
+            File.WriteAllText(filepath, json);
         }
     }
 }
