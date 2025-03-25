2dcc9e7569ef28efa6eac6a1de04f7ab27c92058
DarkCat <Nallblack06264@gmail.com>
2025-03-25 20:03:38 +0900

    Reapply "Добавил фичу на загрузку без основного расписания (1 день)"
    This reverts commit 6318e23f9ae4c3402f2948c7c20076f38b204a06.

---
+++ Classes/JsonHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using unloadSchedule.MVVM.Model;
+
+namespace unloadSchedule.Classes
+{
+    public class JsonHandler
+    {
+        static string filecrnt = @"Jsons\currentTask.json";
+        public string ReadCurrentJson()
+        {
+            string jsonFile = File.ReadAllText(filecrnt);
+            dynamic json = JsonConvert.DeserializeObject<dynamic>(jsonFile);
+            try
+            {
+                string FileName = json.CurrentFile;
+                if (string.IsNullOrEmpty(FileName))
+                {
+                    return string.Empty;
+                }
+                else
+                { return FileName; }
+            }
+            catch { return string.Empty; }
+        }
+    }
+}
